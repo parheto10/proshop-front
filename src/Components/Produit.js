5d6a26a9d07c08c2f480073a5f110a4cab4b7480
parheto10 <parheto10@gmail.com>
2021-06-25 10:55:02 +0000

    first commit

---
+++ src/Components/Produit.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+function Produits() {
+    return (
+        <Card className="my-3 p-3 rounded">
+            produit
+        </Card>
+    )
+
+}
+
+export default Produits;
---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import products from "../../products";
+
+function Home() {
+    return (
+        <div>
+            <h1>Produits Récents </h1>
+        </div>
+    )
+
+}
+
+export default Home;
